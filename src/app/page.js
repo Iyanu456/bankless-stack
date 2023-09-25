b546d74793204924effa155397fd2b7429a813c5
Iyanu <oyerindei13@gmail.com>
2023-09-25 19:57:47 +0000

    code sandbox

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,13 @@ import Navigation from './Navigation'
 export default function Home() {
   return (
     <main>
-      <div className='hero'><Navigation /></div>
+      <div>
+        <Navigation />
+        <div className="hero">
+        <h1 className="main-heading">Expand Your <span><br/>Dev Journey</span></h1>
+        <p><i>{`There are only 10 types of people in this world: those who know binary and those who don't`}</i></p>
+      </div>
+      </div>
     </main>
   )
 }
