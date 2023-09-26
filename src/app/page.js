ba995cdac8de0fde7397c04740dae0f13733c17d
Iyanu456 <oyerindei13@gmail.com>
2023-09-26 17:53:21 +0100

    fixing carousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,38 @@
-import Image from 'next/image'
-import Navigation from './Navigation'
+import Image from "next/image";
+import Navigation from "../components/Navigation";
+import Carousel from "../components/carousel";
 
 export default function Home() {
-  return (
-    <main>
-      <div>
-        <Navigation />
-        <div className="hero">
-        <h1 className="main-heading">Expand Your <span><br/>Dev Journey</span></h1>
-        <p><i>{`There are only 10 types of people in this world: those who know binary and those who don't`}</i></p>
-      </div>
-      </div>
-    </main>
-  )
+	const stacks = ["solidity", "front-end", "back-end"];
+	return (
+		<main>
+			<div className="page-container">
+				<Navigation />
+				<div className="hero">
+					<h1 className="main-heading">
+						Expand Your{" "}
+						<span>
+							<br />
+							Dev Journey
+						</span>
+					</h1>
+					<p>
+						<i>
+							There are only 10 types of people in this
+							<br /> world: those who know binary and <br />
+							those who {`don't`}
+						</i>
+					</p>
+					<h3
+						style={{
+							fontFamily: "D3 Euronism",
+							letterSpacing: "8px",
+						}}>
+						Stacks
+					</h3>
+				</div>
+				<Carousel items={stacks} />
+			</div>
+		</main>
+	);
 }
